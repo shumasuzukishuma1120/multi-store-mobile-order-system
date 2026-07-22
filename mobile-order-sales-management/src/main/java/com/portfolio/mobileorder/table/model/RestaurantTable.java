d14b0056db582ff1f60777ef3061473501251951
shuma_suzuki-admin <shuma.suzuki.shuma1120@gmail.com>
2026-07-22 21:44:34 +0900

    test

--- mobile-order-sales-management/src/main/java/com/portfolio/mobileorder/table/model/RestaurantTable.java
+++ mobile-order-sales-management/src/main/java/com/portfolio/mobileorder/table/model/RestaurantTable.java
@@ -2,6 +2,7 @@ package com.portfolio.mobileorder.table.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDateTime;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RestaurantTable {
 
     private Long id;
